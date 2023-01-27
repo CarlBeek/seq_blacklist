796b2e031f5bf26ac79ec4c11a07b581a3d40c83
Carl Beekhuizen <carl@ethereum.org>
2023-01-27 14:56:29 +0100

    Verify file age as sainty check

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-from typing import Any, Dict, List, Sequence
 import json
+import os
+
+from typing import Any, Dict, List, Sequence
+
+def verify_file_age(trans_path: str, contrib_path:str) -> None:
+    '''
+    Verifies that the transcript was created after the contribution export
+    to ensure that no contributors are accidentally deleted.
+    '''
+    trans_creation_time = os.stat(trans_path).st_birthtime
+    contrib_creation_time = os.stat(contrib_path).st_birthtime
+    assert trans_creation_time > contrib_creation_time
 
 def load_transcript(path: str) -> List[Any]:
     with open(path) as json_file:
@@ -28,6 +39,7 @@ def save_str(str: str, path: str) -> None:
         f.write(str)
 
 def main(trans_path: str='./transcript.json', contrib_path: str='./contributions.json', output_sql=True) -> None:
+    verify_file_age(trans_path, contrib_path)
     trans_raw = load_transcript(trans_path)
     contrib_raw = load_contributions(contrib_path)
     transcript = transcript_to_participants(trans_raw)
