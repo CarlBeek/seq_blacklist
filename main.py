7f06f5f1b06e2e3958588a2e38c2dd5ed97b3e57
Carl Beekhuizen <carl@ethereum.org>
2023-04-20 08:21:20 +0900

    fetches missing ens names with cache

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import click
 import json
 import os
 
+from tqdm import tqdm
 from typing import Any, Dict, List, Sequence
+from web3 import Web3
+from ens import ENS
+
+W3_RPC_PROVIDER = 'https://rpc.ankr.com/eth'
+# W3_RPC_PROVIDER = 'https://rpc.flashbots.net'
 
 def verify_file_age(trans_path: str, contrib_path:str) -> None:
     '''
@@ -55,6 +61,34 @@ def clearlist_intersection(blacklist: List[str], clearlist: List[str]) -> List[s
     return act_blacklisted
 
 
+def calculate_clearlist_from_ens(blacklist: List[str], ens_cache_path: str) -> List[str]:
+    w3_provider = Web3.HTTPProvider(W3_RPC_PROVIDER)
+    ns = ENS(w3_provider)
+    get_ens_name = lambda id: ns.name(id[4:]) or ''
+
+    ens_cache = {}
+    if os.path.exists(ens_cache_path):
+        with open(ens_cache_path) as f:
+            ens_cache = json.load(f)
+
+    def find_in_cache(p_id: str) -> str:
+        if p_id not in ens_cache:
+            ens_cache[p_id] = get_ens_name(p_id)
+        return ens_cache[p_id]
+    
+    clearlist = []
+    for i, p_id in tqdm(enumerate(blacklist), total=len(blacklist)):
+        if p_id[:4] != 'eth|':
+            continue
+        ens = find_in_cache(p_id)
+        if ens != '':
+            clearlist.append(ens)
+        if i % 32 == 0:
+            with open(ens_cache_path, 'w+') as f:
+                json.dump(ens_cache, f, indent=4)
+    return clearlist
+
+
 def print_blacklist_stats(
         transcript_participants: Sequence[str],
         contribution_participants: Sequence[str],
@@ -91,7 +125,9 @@ def print_clearlist_stats(
 @click.option('--output-sql/--no-output-sql', default=True, help='Whether to output an SQL file.')
 @click.option('--output-json/--no-output-json', default=True, help='Whether to output a JSON file.')
 @click.option('--clearlist-path', default=None, help='Path to the file containing potential blacklisted handles and ethereum addresses.')
-def main(trans_path: str, contrib_path: str, output_sql: bool, output_json: bool, clearlist_path: str) -> None:
+@click.option('--ens-cache-path', default='./ens_cache.json', help='Path to the file containing the ENS cache.')
+@click.option('--clearlist-from-ens/--no-clearlist-from-ens', default=False, help='Whether to generate the clearlist from the ENS cache.')
+def main(trans_path: str, contrib_path: str, output_sql: bool, output_json: bool, clearlist_path: str, ens_cache_path: str, clearlist_from_ens: bool) -> None:
     verify_file_age(trans_path, contrib_path)
     trans_raw = load_transcript(trans_path)
     contrib_raw = load_contributions(contrib_path)
@@ -102,13 +138,16 @@ def main(trans_path: str, contrib_path: str, output_sql: bool, output_json: bool
     if output_json:
         json_blacklist = generate_blacklist_json(blacklist)
         save_str(json_blacklist, './blacklist.json')
+    clearlist = None
     if clearlist_path is not None:
         potential_clearlist = get_clearlist(clearlist_path)
         clearlist = clearlist_intersection(blacklist, potential_clearlist)
         print_clearlist_stats(blacklist, potential_clearlist, clearlist)
+    if clearlist_from_ens:
+        clearlist = calculate_clearlist_from_ens(blacklist, ens_cache_path)
     if output_sql:
         sql = generate_blacklist_flush_sql(blacklist)
-        if clearlist_path is not None:
+        if clearlist is not None:
             sql = generate_blacklist_flush_sql(clearlist)
         save_str(sql, './blacklist_flush.sql')
 
